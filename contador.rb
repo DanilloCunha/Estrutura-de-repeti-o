2ff62092457ba92d0a943ae1d7465ea7a18277e2
Danillo Cunha <107063327+DanilloCunha@users.noreply.github.com>
2024-11-06 16:27:13 -0300

    Update contador.rb

--- contador.rb
+++ contador.rb
@@ -6,4 +6,14 @@ while contador <= 10
 end
 puts "Terminei de contar"
 
+______________________________________________________________________________
+mesma idéia, porem, contando de forma decrescente de 10 para 0 
+
+contador = 10
+while contador >= 10
+  puts contador
+  contador -= 1
+
+end
+puts "Terminei de contar"
 
